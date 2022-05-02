76b9b71f354d97f9d97e831161497c12936529cb
rahulsingh34 <rahulsingh342000@gmail.com>
2022-05-02 19:43:52 -0400

    Calculator functions added

---
+++ script.js
@@ -0,0 +1,23 @@
+function add(a, b) {
+	let c = a + b;
+	return c;
+}
+
+function subtract(a, b) {
+	let c = a - b;
+	return c;
+}
+
+function multiply(a, b) {
+	let c = a * b;
+	return c;
+}
+
+function divide(a, b) {
+	let c = a / b;
+	return c;
+}
+
+function operate(operator, a, b) {
+	return operator(a,b)
+}
