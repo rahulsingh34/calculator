95692cac296321438667743b4b7fce53efd96ae7
rahulsingh34 <rahulsingh342000@gmail.com>
2022-05-06 20:19:06 -0400

    improvement on html and css added more functions to collect first number and operator

--- script.js
+++ script.js
@@ -22,14 +22,65 @@ function operate(operator, a, b) {
 	return operator(a, b)
 }
 
-const mainDisplay = document.getElementById("bottom");
+let number1 = '';
+let operation = '';
+let number2 = '';
+let number3 = '';
+
+const mainDisplay = document.getElementById('bottom');
 const buttons = document.querySelectorAll('.btn');
 
-mainDisplayValue = [];
+mainDisplayValue1 = [];
+mainDisplayValue2 = [];
 
 buttons.forEach((button) => {
 	button.addEventListener('click', () => {
-		mainDisplayValue.push(button.innerText);
-		mainDisplay.innerText = mainDisplayValue;
+		mainDisplayValue1.push(button.innerText);
+		mainDisplay.innerText = mainDisplayValue1.slice(0,10).join('');
+		if (operation == '') {
+			number1 = mainDisplayValue1.slice(0,10).join('');
+		}
+		else if (operation != '') {
+			mainDisplayValue2.push(button.innerText);
+			mainDisplay.innerText = mainDisplayValue2.slice(0,10).join('');
+		}
 	});
 });
+
+const actions = document.querySelectorAll('.btn-o');
+topDisplay = document.getElementById('top');
+topDisplayValue = [];
+
+actions.forEach((action) => {
+	action.addEventListener('click', () => {
+		if(operation == '') {
+			operation = action.innerHTML;
+			topDisplayValue.push(number1, operation);
+			topDisplay.innerText = topDisplayValue.join(' ');
+		}
+		else if (operation != '' && number2 == '') {
+			operation = action.innerHTML;
+			topDisplayValue[1] = operation;
+			topDisplay.innerText = topDisplayValue.join(' ');
+		}	
+		else if (operation != '' && number2 != '') {
+			number3 = operate(performOperation, number1, number2);
+		}
+	});
+});
+
+
+function performOperation(operator) {
+	if(operator == '+') {
+		return add;
+	}
+	else if (operator == '-') {
+		return subtract;
+	}
+	else if (operator == '&times') {
+		return multiply;
+	}
+	else if (operator == '&#247') {
+		return divide;
+	}
+}
